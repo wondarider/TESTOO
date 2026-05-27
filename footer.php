fb63cb7443a495fe86881983e12f5c68d2082ea2
wondarider <wondarider@gmail.com>
2026-05-27 22:22:07 +0800

    f

---
+++ footer.php
@@ -0,0 +1,26 @@
+<footer class="site-footer">
+    
+    <div class="footer-links-row">
+      <h4>Related Websites:</h4>
+      <a href="#">Related Site A</a>
+      <a href="#">Related Site B</a>
+      <a href="#">Related Site C</a>
+    </div>
+
+    <hr class="footer-divider">
+
+    <div class="footer-bottom-row">
+      <nav class="footer-nav">
+        <a href="#">About Us</a>
+        <a href="#">License</a>
+        <a href="#">Contact</a>
+      </nav>
+      
+      <div class="social-links">
+        <a href="#">X</a>
+        <a href="#">LinkedIn</a>
+        <a href="#">GitHub</a>
+      </div>
+    </div>
+
+  </footer>
